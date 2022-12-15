2305300097856ad06cfc869b405b2871716c59a9
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-14 20:23:47 -0700

    Add saved display functionality

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var homeButton = document.querySelector("#home");
 var headers = document.querySelector("header");
 var optionBox = document.querySelector(".option-box");
 var msgDisplayBox = document.querySelector(".msg-display-box");
+var favDisplayBox = document.querySelector(".fav-display");
 
 var favButtonImg = document.querySelector("#fav-button");
 var meditateIcon = document.querySelector(".meditation-icon");
@@ -20,7 +21,8 @@ var currentFavorited = false;
 //event listeners
 submitButton.addEventListener("click", outputRandom);
 favoriteButton.addEventListener("click", favoriteMessage);
-viewFavsButton.addEventListener("click", switchPages);
+viewFavsButton.addEventListener("click", showSaved);
+homeButton.addEventListener("click", switchPages);
 
 
 //event handlers
@@ -81,9 +83,22 @@ function favoriteMessage() {
 };
 
 function switchPages() {
-    viewFavsButton.classList.add("hidden");
-    homeButton.classList.remove("hidden");
-    headers.classList.add("hidden");
-    optionBox.classList.add("hidden");
-    msgDisplayBox.classList.add("hidden");
+    favDisplayBox.innerHTML = "";
+    viewFavsButton.classList.toggle("hidden");
+    homeButton.classList.toggle("hidden");
+    headers.classList.toggle("hidden");
+    optionBox.classList.toggle("hidden");
+    msgDisplayBox.classList.toggle("hidden");
+    favDisplayBox.classList.toggle("hidden");
+};
+
+function showSaved() {
+    switchPages();
+
+    for (var i = 0; i < favorites.length; i++) {
+        var createBox = document.createElement("div");
+        createBox.setAttribute("class", "faved-msg-box");
+        createBox.innerText = favorites[i];
+        favDisplayBox.appendChild(createBox);
+    };
 };
