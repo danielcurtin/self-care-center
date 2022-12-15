04496c308c7ff146fa9ffc8d5229bbe5c060a69f
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-14 19:44:03 -0700

    Add nav buttons and page switch functionality

--- main.js
+++ main.js
@@ -2,31 +2,41 @@ var affirmButton = document.querySelector("#affirmation");
 var mantraButton = document.querySelector("#mantra");
 var submitButton = document.querySelector("#receive-msg");
 var favoriteButton = document.querySelector(".favorite");
+var viewFavsButton = document.querySelector("#view-favs")
+var homeButton = document.querySelector("#home");
+
+var headers = document.querySelector("header");
+var optionBox = document.querySelector(".option-box");
+var msgDisplayBox = document.querySelector(".msg-display-box");
+
 var favButtonImg = document.querySelector("#fav-button");
 var meditateIcon = document.querySelector(".meditation-icon");
-var displayedMessage = document.querySelector(".displayed-message");
+var visibleMsg = document.querySelector(".displayed-message");
 
 var favorites = [];
 var currentFavorited = false;
 
+
 //event listeners
 submitButton.addEventListener("click", outputRandom);
 favoriteButton.addEventListener("click", favoriteMessage);
+viewFavsButton.addEventListener("click", switchPages);
 
-//functions
+
+//event handlers
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
 };
 
 function displayMessage() {
     meditateIcon.classList.add("hidden");
-    displayedMessage.classList.remove("hidden");
+    visibleMsg.classList.remove("hidden");
     favoriteButton.classList.remove("hidden");
 };
 
 function displayIcon() {
     meditateIcon.classList.remove("hidden");
-    displayedMessage.classList.add("hidden");
+    visibleMsg.classList.add("hidden");
     favoriteButton.classList.add("hidden");
 };
 
@@ -34,11 +44,11 @@ function outputRandom() {
     disableFav();
     if (mantraButton.checked) {
         displayMessage();
-        displayedMessage.innerText = mantras[getRandomIndex(mantras)];
+        visibleMsg.innerText = mantras[getRandomIndex(mantras)];
         mantraButton.checked = false;
     } else if (affirmButton.checked) {
         displayMessage();
-        displayedMessage.innerText = affirmations[getRandomIndex(affirmations)];
+        visibleMsg.innerText = affirmations[getRandomIndex(affirmations)];
         affirmButton.checked = false;
     } else {
         displayIcon();
@@ -56,7 +66,7 @@ function disableFav() {
 }
 
 function favoriteMessage() {
-    var currentMsg = displayedMessage.innerText;
+    var currentMsg = visibleMsg.innerText;
     if (!currentFavorited) {
         enableFav();
         favorites.push(currentMsg);
@@ -64,8 +74,16 @@ function favoriteMessage() {
         for (var i = 0; i < favorites.length; i++) {
             if (currentMsg === favorites[i]) {
                 favorites.splice(i, 1);
-            }
-        }
+            };
+        };
         disableFav();
     };
+};
+
+function switchPages() {
+    viewFavsButton.classList.add("hidden");
+    homeButton.classList.remove("hidden");
+    headers.classList.add("hidden");
+    optionBox.classList.add("hidden");
+    msgDisplayBox.classList.add("hidden");
 };
